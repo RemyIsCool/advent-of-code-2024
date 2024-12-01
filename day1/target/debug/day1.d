c88f7e470f60bc45e89ab1576e730186db04fd82
RemyIsCool <97812130+RemyIsCool@users.noreply.github.com>
2024-12-01 07:15:11 -0800

    Delete day1/target directory

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/remy/Documents/advent-of-code-2024/day1/target/debug/day1: /home/remy/Documents/advent-of-code-2024/day1/src/main.rs
--- day1/target/debug/deps/day1-7d4cab7eed5f940c.d
+++
@@ -1,5 +0,0 @@
-/home/remy/Documents/advent-of-code-2024/day1/target/debug/deps/libday1-7d4cab7eed5f940c.rmeta: src/main.rs
-
-/home/remy/Documents/advent-of-code-2024/day1/target/debug/deps/day1-7d4cab7eed5f940c.d: src/main.rs
-
-src/main.rs:
