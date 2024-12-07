70817ae9404da95a90451dca4c4e8f9020411f6c
RemyIsCool <remyrfouquette@gmail.com>
2024-12-07 08:25:26 -0800

    day 6 - keeping up the streak of brute force solutions! just binary counting and no optimizations whatsoever.

---
+++ day7/part1.py
@@ -0,0 +1,39 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+
+answer = 0
+
+for line in lines:
+	test, equation = line.split(":")
+	test = int(test)
+	equation = equation.strip()
+	space_count = equation.count(" ")
+	binary_operators_max = int("1" * space_count, 2)
+
+	possible = False
+	
+	for x in range(binary_operators_max+1):
+		binary_string = bin(x)[2:]
+		if len(binary_string) < space_count:
+			binary_string = "0" * (space_count - len(binary_string)) + binary_string
+		operators_string = binary_string.replace("0", "*").replace("1", "+")
+
+		with_operators = "(" * space_count
+
+		for char in equation:
+			if char != " ":
+				with_operators += char
+				continue
+
+			with_operators += ")" + operators_string[0]
+
+			operators_string = operators_string[1:]
+
+		if eval(with_operators) == test:
+			possible = True
+			break
+	
+	if possible:
+		answer += test
+
+print(answer)
