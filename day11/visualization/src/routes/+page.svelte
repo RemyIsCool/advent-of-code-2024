19057b05870ab117ea42a4d306a6490e9d4a77f8
RemyIsCool <remyrfouquette@gmail.com>
2024-12-11 18:01:52 -0800

    improvements (?) to the visualization for day 11

--- day11/visualization/src/routes/+page.svelte
+++ day11/visualization/src/routes/+page.svelte
@@ -1,42 +1,32 @@
 <script lang="ts">
-	import { flip } from 'svelte/animate';
-	import { slide } from 'svelte/transition';
+	import { fade, slide } from 'svelte/transition';
 
-	type Stone = {
-		value: number;
-		id: number;
-	};
+	let stones = $state([125, 17]);
+	let done = $state(false);
 
-	let stones = $state<Stone[]>([125, 17].map((num) => ({ value: num, id: Math.random() })));
+	const iterations = 5;
 
 	$effect(() => {
 		let count = 0;
 
 		const interval = setInterval(() => {
-			if (count >= 25) {
+			if (count >= iterations - 1) {
 				clearInterval(interval);
+				setTimeout(() => (done = true), 3000);
 			}
 
-			const nextStones: Stone[] = [];
+			const nextStones: number[] = [];
 
-			for (const stoneObj of stones) {
-				const stone = stoneObj.value;
-
+			for (const stone of stones) {
 				if (stone === 0) {
-					nextStones.push({ ...stoneObj, value: 1 });
+					nextStones.push(1);
 				} else if (stone.toString().length % 2 === 0) {
 					nextStones.push(
-						{
-							value: parseInt(stone.toString().slice(0, stone.toString().length / 2)),
-							id: Math.random()
-						},
-						{
-							value: parseInt(stone.toString().slice(stone.toString().length / 2)),
-							id: Math.random()
-						}
+						parseInt(stone.toString().slice(0, stone.toString().length / 2)),
+						parseInt(stone.toString().slice(stone.toString().length / 2))
 					);
 				} else {
-					nextStones.push({ ...stoneObj, value: stone * 2024 });
+					nextStones.push(stone * 2024);
 				}
 			}
 
@@ -49,23 +39,31 @@
 	});
 </script>
 
-<div class="grid">
-	<div class="col-span-full row-span-full grid min-h-screen place-content-center pb-48">
-		<h1 class="mx-auto mb-12 w-3/4 text-center text-5xl font-bold">
-			Advent of Code - Day 11: Plutonian Pebbles
-		</h1>
-		<div class="flex flex-wrap justify-center gap-6">
-			{#each stones as stone (stone.id)}
-				<p
-					animate:flip
-					in:slide={{ axis: 'x' }}
-					class="rounded-xl border-2 border-slate-600 px-10 py-6 text-4xl"
-				>
-					{stone.value}
-				</p>
-			{/each}
+<div class="grid min-h-screen px-8 py-10">
+	{#if done}
+		<p
+			transition:fade
+			class="col-span-full row-span-full mb-12 grid place-content-center text-center text-5xl"
+		>
+			The amount of stones after {iterations} blinks is: <strong>{stones.length}</strong>
+		</p>
+	{:else}
+		<div transition:fade class="col-span-full row-span-full grid place-content-center pb-48">
+			<h1 class="mx-auto mb-12 w-3/4 text-center text-5xl font-bold">
+				Advent of Code - Day 11: Plutonian Pebbles
+			</h1>
+			<div class="flex flex-wrap justify-center gap-6">
+				{#each stones as stone}
+					<p
+						in:slide={{ axis: 'x' }}
+						class="rounded-xl border-2 border-slate-600 px-10 py-6 text-4xl"
+					>
+						{stone}
+					</p>
+				{/each}
+			</div>
 		</div>
-	</div>
+	{/if}
 </div>
 
 <style>
