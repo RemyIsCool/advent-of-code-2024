2c0e810fd6807197153e184161542f931777f62d
RemyIsCool <remyrfouquette@gmail.com>
2024-12-18 16:58:58 -0800

    binary search to speed up day 18

--- day18/day18.py
+++ day18/day18.py
@@ -4,7 +4,8 @@ from typing import TypeAlias
 from tqdm import tqdm
 
 Position: TypeAlias = tuple[int, int]
-Map: TypeAlias = list[list[bool]]
+
+SIZE = 70
 
 
 def read_input() -> list[str]:
@@ -24,37 +25,21 @@ def parse_input(input: list[str]) -> list[Position]:
     return positions
 
 
-def generate_map(positions: list[Position], byte_count: int, grid_range: int) -> Map:
-    memory_map = [[False for _ in range(grid_range + 1)] for _ in range(grid_range + 1)]
-
-    for x, y in positions[:byte_count]:
-        memory_map[y][x] = True
-
-    return memory_map
-
-
-def print_map(map: Map):
-    for line in map:
-        for place in line:
-            print("#" if place else ".", end="")
-        print()
-
-
-def get_neighbors(position: Position, map: Map) -> list[Position]:
+def get_neighbors(position: Position, walls: list[Position]) -> list[Position]:
     neighbors: list[Position] = []
 
     for direction_x, direction_y in ((1, 0), (0, 1), (-1, 0), (0, -1)):
         offsetted_x, offsetted_y = position[0] + direction_x, position[1] + direction_y
 
         if (
-            offsetted_x >= len(map[0])
-            or offsetted_y >= len(map)
+            offsetted_x > SIZE
+            or offsetted_y > SIZE
             or offsetted_x < 0
             or offsetted_y < 0
         ):
             continue
 
-        if map[offsetted_y][offsetted_x]:
+        if (offsetted_x, offsetted_y) in walls:
             continue
 
         neighbors.append((offsetted_x, offsetted_y))
@@ -62,13 +47,13 @@ def get_neighbors(position: Position, map: Map) -> list[Position]:
     return neighbors
 
 
-def dijkstras(map: Map) -> float:
+def find_distance(walls: list[Position]) -> float:
     distances: dict[Position, float] = {}
     previous: dict[Position, Position | None] = {}
     queue: list[Position] = []
 
-    for y, row in enumerate(map):
-        for x in range(len(row)):
+    for x in range(SIZE + 1):
+        for y in range(SIZE + 1):
             position: Position = (x, y)
             distances[position] = inf
             previous[position] = None
@@ -80,7 +65,7 @@ def dijkstras(map: Map) -> float:
         current = min(queue, key=lambda position: distances[position])
         queue.remove(current)
 
-        for neighbor in get_neighbors(current, map):
+        for neighbor in get_neighbors(current, walls):
             if neighbor not in queue:
                 continue
 
@@ -90,14 +75,14 @@ def dijkstras(map: Map) -> float:
                 distances[neighbor] = distance_from_current
                 previous[neighbor] = current
 
-    end_position = (len(map[0]) - 1, len(map) - 1)
+    end_position = (SIZE, SIZE)
 
     assert distances[end_position] != inf, "No path found"
 
     return distances[end_position]
 
 
-def bfs(map: Map) -> bool:
+def exists_path(walls: list[Position]) -> bool:
     start: Position = (0, 0)
     visited: list[Position] = [start]
     queue: list[Position] = [start]
@@ -105,8 +90,8 @@ def bfs(map: Map) -> bool:
     while queue:
         current = queue.pop(0)
 
-        for neighbor in get_neighbors(current, map):
-            if neighbor == (len(map[0]) - 1, len(map) - 1):
+        for neighbor in get_neighbors(current, walls):
+            if neighbor == (SIZE, SIZE):
                 return True
 
             if neighbor not in visited:
@@ -116,22 +101,34 @@ def bfs(map: Map) -> bool:
     return False
 
 
+def find_blocking_byte(walls: list[Position], low: int, high: int) -> int:
+    if low + 1 == high:
+        return high
+
+    if low == high:
+        return high - 1
+
+    midpoint = (high + low) // 2
+
+    if exists_path(walls[:midpoint]):
+        return find_blocking_byte(walls, midpoint + 1, high)
+    else:
+        return find_blocking_byte(walls, low, midpoint - 1)
+
+
 def part1():
     input = read_input()
-    positions = parse_input(input)
+    walls = parse_input(input)
 
-    map = generate_map(positions, 1024, 70)
-    return dijkstras(map)
+    return find_distance(walls[:1024])
 
 
 def part2():
     input = read_input()
-    positions = parse_input(input)
+    walls = parse_input(input)
+    blocking_index = find_blocking_byte(walls, 0, len(walls) - 1)
 
-    for x in tqdm(range(1024, len(positions))):
-        map = generate_map(positions, x, 70)
-        if not bfs(map):
-            return positions[x - 1]
+    return ",".join(list(map(str, walls[blocking_index])))
 
 
 print("Part 1:", part1())
